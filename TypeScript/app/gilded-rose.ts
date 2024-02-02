2ec25b4e7514c1d292acbfe3ba0a3379a0bb3d05
Felix Nehrke <felix@nehrke.info>
2024-02-02 14:32:00 +0100

    refactor: extract method to update default qualities

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -38,18 +38,23 @@ export class GildedRose {
     if (item.name == 'Sulfuras, Hand of Ragnaros' as string) {
       return;
     } else {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0) {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
-      }
+      this.updateDefaultQuality(item);
       return;
     }
   }
+
+  private updateDefaultQuality(item: Item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
+      }
+    }
+  }
+
   private updateQualityOfBackstagePasses(item: Item) {
     if (item.quality < 50) {
       item.quality = item.quality + 1
