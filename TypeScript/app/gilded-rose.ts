35ab462e9872dcfeb61bd5df72a31b60447996e7
Felix Nehrke <felix@nehrke.info>
2024-02-02 13:40:01 +0100

    refactor: remove empty branch

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -37,11 +37,8 @@ export class GildedRose {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (false) {
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
         }
       }
       return;
