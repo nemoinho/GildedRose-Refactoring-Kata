c17950f2d3ca092c49ad6b530f7c09439be296e8
Felix Nehrke <felix@nehrke.info>
2024-02-02 13:40:33 +0100

    refactor: remove obsolete if

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -70,17 +70,14 @@ export class GildedRose {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (true) {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
+        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0) {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              item.quality = item.quality - 1
             }
-          } else {
-            item.quality = item.quality - item.quality
           }
         } else {
+          item.quality = item.quality - item.quality
         }
       }
       return;
