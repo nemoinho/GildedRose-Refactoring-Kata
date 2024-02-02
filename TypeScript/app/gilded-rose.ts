299465c279c2b979920a9695a7bb495bd21d7865
Felix Nehrke <felix@nehrke.info>
2024-02-02 14:01:42 +0100

    refactor: remove dead path

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -33,11 +33,6 @@ export class GildedRose {
     }
     if (item.name == 'Backstage passes to a TAFKAL80ETC concert' as string) {
       if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1
