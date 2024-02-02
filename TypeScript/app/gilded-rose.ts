d217e1ae40d59e4af4cba90164f7c7187b769b6c
Felix Nehrke <felix@nehrke.info>
2024-02-02 13:27:03 +0100

    refactor: add hint to compiler that item.name is string

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -27,7 +27,7 @@ export class GildedRose {
   }
 
   private foo(item: Item) {
-    if (item.name == 'Aged Brie') {
+    if (item.name == 'Aged Brie' as string) {
       this.bar(item);
       return;
     } else {
